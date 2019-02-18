069755979c931176524744be4499db6a359d4e80
iamcco <ooiss@qq.com>
2019-02-18 15:32:09 +0800

    add package main

--- src/loader/preloadmodules.ts
+++ src/loader/preloadmodules.ts
@@ -3,11 +3,13 @@ import log4js from 'log4js'
 import msgpackLite from 'msgpack-lite'
 import * as neovim from 'neovim'
 import * as tslib from 'tslib'
+import Plugin from '../attach/plugin'
 
 export default {
   neovim,
   log4js,
   tslib,
   'msgpack-lite': msgpackLite,
-  'fast-glob': fastGlob
+  'fast-glob': fastGlob,
+  'sran.nvim': Plugin
 }
